4902961a46ef2eb2929bfb14e668d31a50ab941a
jbenjaminy <young.bejamin@gmail.com>
2016-07-27 16:54:58 -0500

    Adds updates to test-overlay

--- js/overlay.jsx
+++ js/overlay.jsx
@@ -8,13 +8,13 @@ var Overlay = function (props) {
       <button onClick={props.onButtonClick} className={instructionsButton}>Instructions</button>
       <div className={classes}>
         <h3>How do I play?</h3>
-        <p>This is a 'Hot-or-Cold' number-guessing game. Here's how it works: </p>
+        <p>This is a 'Hot-or-Cold' number-guessing game. Here's how it works:</p>
         <ul>
           <li>1. The computer picks a number between 1 and 100.</li>
           <li>2. The user enters guesses until they find the hidden number.</li>
           <li>3. For each guess the user will be presented with feedback indicating how close their guess is from the hidden number.</li>
         </ul>
-        <h3>So, Are you ready?</h3>
+        <h3>So, are you ready?</h3>
         <button onClick={props.onButtonClick}>Got It!</button>
       </div> 
     </div>
