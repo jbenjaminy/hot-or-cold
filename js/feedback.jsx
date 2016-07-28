65d2d716ec586b1efbf576b51ed97cb9ff9f9b0f
Sierra Gregg <sierragregg@Gmail.com>
2016-07-28 08:48:44 -0500

    Added additional jsDoc comments to Feedback

--- js/feedback.jsx
+++ js/feedback.jsx
@@ -1,8 +1,10 @@
 var React = require('react');
 
 /**
- * @Feedback - Component for rendering the feedback string 
+ * Component for rendering the feedback string 
  * in a response to the user's guess.
+ * @constructor
+ * @param {Object} props Contains a property for the user's feedback
  */
 var Feedback = function (props) {
 	return <h3 className="feedback">{props.text}</h3>
